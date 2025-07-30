2bfb617a42e3a344759dc1831a2ffb57b8c8e371
EenTweeDrie <eentweedrie@yandex.ru>
2025-07-30 12:51:18 +0300

    Улучшены метрики в классе MLValidator: добавлены параметры average='weighted' и zero_division=0 для функций precision, recall и f1_score для более корректной оценки при наличии нулевых значений.

--- pc_forestry/ml/ml_validator.py
+++ pc_forestry/ml/ml_validator.py
@@ -50,9 +50,9 @@ class MLValidator:
         """
         metrics = {
             "accuracy": accuracy_score(y_true, y_pred),
-            "precision": precision_score(y_true, y_pred),
-            "recall": recall_score(y_true, y_pred),
-            "f1": f1_score(y_true, y_pred),
+            "precision": precision_score(y_true, y_pred, average='weighted', zero_division=0),
+            "recall": recall_score(y_true, y_pred, average='weighted', zero_division=0),
+            "f1": f1_score(y_true, y_pred, average='weighted', zero_division=0),
             "mcc": matthews_corrcoef(y_true, y_pred),
         }
         return metrics
